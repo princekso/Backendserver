0f6736fc3862ba7038f660ada953dac5fd371edf
princekso <princekgupta00000000000@gmail.com>
2025-07-13 07:01:08 +0530

    Update unoGame.js

--- unoGame.js
+++ unoGame.js
@@ -1,4 +1,4 @@
-// 📁 server/unoGame.js
+
 const colors = ['Red', 'Green', 'Blue', 'Yellow'];
 const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const specials = ['Skip', 'Reverse', '+2'];
